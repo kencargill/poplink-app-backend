0f3fd35722c201e62cb6af0a8254c50b7041341b
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-04-22 22:38:15 -0400

    added profile controller, services

--- src/models/profile.model.ts
+++ src/models/profile.model.ts
@@ -1,1 +1,15 @@
-export {};
+import { model, Schema } from 'mongoose';
+import { Profile } from '../types/profile.types';
+
+const profileSchema = new Schema<Profile>(
+  {
+    userId: { type: String, required: true },
+    profileUsername: { type: String, required: true, unique: true },
+    profileName: { type: String, required: true },
+    profileDescription: { type: String },
+    profilePhotoUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const profileModel = model<Profile>('Profile', profileSchema);
