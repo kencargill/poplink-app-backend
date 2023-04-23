6d0ac5da36fcd25342e278cb3f9edb62acf8f60e
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-04-23 11:27:25 -0400

    updated user controller, service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,31 @@
+import { userModel } from '../models/user.model';
 import { User } from '../types/user.types';
 
-async function getUsers() {}
+async function getUsers() {
+  const users = await userModel.find();
+  return users;
+}
 
-async function getUser(id: string) {}
+async function getUser(id: string) {
+  const user = await userModel.findById(id);
+  return user;
+}
 
-async function addUser(data: Partial<User>) {}
+async function addUser(data: Partial<User>) {
+  const newUser = await userModel.create(data);
+  return newUser;
+}
 
-async function updateUser(id: string, data: Partial<User>) {}
+async function updateUser(id: string, data: Partial<User>) {
+  const updatedUser = await userModel.findByIdAndUpdate(id, data, {
+    new: true,
+  });
+  return updatedUser;
+}
 
-async function deleteUser(id: string) {}
+async function deleteUser(id: string) {
+  return await userModel.findByIdAndDelete(id);
+}
 
 export const usersService = {
   getUsers,
