afe433701213a9b3d0f38468b1074b58bc2210fc
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-04-22 22:32:14 -0400

    updated links controller, services

--- src/services/link.service.ts
+++ src/services/link.service.ts
@@ -1,20 +1,54 @@
+import { linkModel } from '../models/link.model';
 import { Link } from '../types/link.types';
 
-async function getLinks() {}
+async function getLinks() {
+  const links = await linkModel.find().lean();
+  return links;
+}
 
-async function getUserLinks(userId: string) {}
+async function getUserLinks(userId: string) {
+  const links = await linkModel.find({ userId }).lean();
+  return links
+}
 
-async function getProfileLinks(profileId: string) {}
+async function getProfileLinks(profileId: string) {
+  const links = await linkModel
+  .find({ profileId })
+  .sort({ createdAt: 'desc' })
+  .lean();
+  return links;
+}
 
-async function getLinksByUsername(profileUsername: string) {}
+async function getLinksByUsername(profileUsername: string) {
+  const links = await linkModel
+  .find({ profileUsername })
+  .sort({ createdAt: 'desc' })
+  .lean();
+  return links;
+}
 
-async function getLink(id: string) {}
+async function getLink(id: string) {
+  const link = await linkModel.findById(id).lean();
+  return link;
+}
 
-async function addLink(data: Partial<Link>) {}
+async function addLink(data: Partial<Link>) {
+  const newUser = await linkModel.create(data);
+  return newUser;
+}
 
-async function updateLink(id: string, data: Partial<Link>) {}
+async function updateLink(id: string, data: Partial<Link>) {
+  const updatedUser = await linkModel
+  .findByIdAndUpdate(id, data, {
+    new: true,
+  })
+  .lean();
+  return updatedUser;
+}
 
-async function deleteLink(id: string) {}
+async function deleteLink(id: string) {
+  return await linkModel.findByIdAndDelete(id);
+}
 
 export const linkService = {
   getLinks,
